0c2a8d506c70653a6f0142363ee2f4b7c7e7a462
Dhen E Padilla <dhenepadilla@gmail.com>
2020-02-21 19:43:43 +0000

    Created initial flows

---
+++ cordapp-template-java/workflows/src/main/java/com/template/flows/IOUFlow.java
@@ -0,0 +1,77 @@
+package com.template.flows;
+
+import com.template.contracts.TemplateContract;
+import com.template.states.IOUState;
+import net.corda.core.contracts.Command;
+import net.corda.core.identity.Party;
+import net.corda.core.transactions.SignedTransaction;
+import net.corda.core.transactions.TransactionBuilder;
+
+import co.paralleluniverse.fibers.Suspendable;
+import net.corda.core.flows.FlowException;
+import net.corda.core.flows.FlowLogic;
+import net.corda.core.flows.InitiatingFlow;
+import net.corda.core.flows.StartableByRPC;
+import net.corda.core.utilities.ProgressTracker;
+
+// ******************
+// * Initiator flow *
+// ******************
+@InitiatingFlow
+@StartableByRPC
+public class IOUFlow extends FlowLogic<Void> {
+    private final Integer iouValue;
+    private final Party otherParty;
+
+    /**
+     * The progress tracker provides checkpoints indicating the progress of the flow to observers.
+     */
+    private final ProgressTracker progressTracker = new ProgressTracker();
+
+    public IOUFlow(Integer iouValue, Party otherParty) {
+        this.iouValue = iouValue;
+        this.otherParty = otherParty;
+    }
+
+    @Override
+    public ProgressTracker getProgressTracker() {
+        return progressTracker;
+    }
+
+    /**
+     * Any state's flow logic is encapsulated within the call() method.
+     */
+    @Suspendable
+    @Override
+    public Void call() throws FlowException {
+        // We retrieve the notary identity from the network map.
+        // The first thing we do in our flow is retrieve the a notary from the
+        // node’s ServiceHub.
+        // ServiceHub.networkMapCache provides information about the
+        // other nodes on the network and the services that they offer.
+        Party notary = getServiceHub().getNetworkMapCache().getNotaryIdentities().get(0);
+
+        // Create the transaction components
+        // (I think getOurIdentity() get's the Flow-caller's Party object
+        IOUState outputState = new IOUState(iouValue, getOurIdentity(), otherParty);
+        Command command = new Command<>(new TemplateContract.Commands.Action(), getOurIdentity().getOwningKey());
+
+        // Create a transaction builder and add the components
+        // Transaction builders take in 'notary' party as the parameter to
+        // instantiate the txBuilder
+        TransactionBuilder txBuilder = new TransactionBuilder(notary)
+                .addOutputState(outputState, TemplateContract.ID)
+                .addCommand(command);
+
+        // Signing the transaction.
+        SignedTransaction signedTx = getServiceHub().signInitialTransaction(txBuilder);
+
+        // Creating a session with the other party.
+        FlowSession otherPartySession = initiateFlow(otherParty);
+
+        // We finalise the transaction and then send it to the counterparty.
+        subFlow(new FinalityFlow(signedTx, otherPartySession));
+
+        return null;
+    }
+}
